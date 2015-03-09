9a64a77089231fee8f3827968c77f2dfbc6651f5
Jahde <jahde@me.com>
2015-03-09 01:37:30 -0400

    added turns

--- ttt.rb
+++ ttt.rb
@@ -1,44 +1,63 @@
 # players, board, position, win/loss track
 class Game
-  attr_accessor :grid, :player1, :player2
+  attr_accessor :grid, :player1, :player2, :computer
 
+  # initialize a new board for the game
   def initialize
     @grid = Board.new
+    @computer = Player.new("computer", "O")
   end
 
   def play
     new_players
     welcome_message
     go_play
+    continue_play
     # show_result
     # show_game_over_message
   end
 
+  # gets the information from the players including name and auto-generated symbol of x or o
   def new_players
     puts "Welcome to the Tic Tac Toe Game X-O-X-O"
     players = []
     puts "Player 1 name:"
     name1 = gets.chomp
-    player1 = Player.new(name1, "X")
-    player1.say_hello
+    @player1 = Player.new(name1, "X")
+    @player1.say_hello
     players << player1
     puts "Player 2 name:"
     name2 = gets.chomp
-    player2 = Player.new(name2, "O")
-    player2.say_hello
+    @player2 = Player.new(name2, "O")
+    @player2.say_hello
     players << player2
   end
 
+  # welcomes players to the game and creates a blank grid to show
   def welcome_message
+    puts "\n"
     puts "The object of the game is to get three spots in a row, diagonally/horizontally/vertically"
     puts "Use the numbers from 1-9 to select your position:"
     puts "\n"
+    # sample layout of grid to show the relevant numbers matched to the cell
     (1..9).each_slice(3) {|a| puts a.join(' | ')}
     puts "\n"
   end
 
+  # starts the game by printing out the blank board
   def go_play
+    puts "Let's start a new game"
     @grid.print_board
+    puts @player1.name + " " + "your turn first!"
+  end
+
+  def continue_play
+    puts "\n"
+    i = 0
+    while i < 9
+      puts "Turn " + (i+1).to_s + ":"
+      i += 1
+    end
   end
 end
 
@@ -61,19 +80,20 @@ end
 class Board
   attr_reader :board, :empty_cell
 
+
   def initialize
     @empty_cell = '-' # board starts with all empty cells
     @board = Array.new(9, @empty_cell) # create a board with 9 empty cells
   end
 
-  def print_board # prints a new board each time
+  def print_board # prints a new blank board each time
     puts "\n"
     @board.each_slice(3) {|a| puts a.join(' | ')}
     puts "\n"
   end
 
-
-  def create_board
+  # updates the board with the relevant positions
+  def update_board
   end
 end
 
